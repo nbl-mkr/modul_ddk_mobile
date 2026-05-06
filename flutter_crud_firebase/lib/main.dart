fd57690d35d4f334ef2689adc4031f3a14178f7a
MNM29 <n481lmaka@gmail.com>
2026-05-06 11:15:24 +0700

    commit

--- flutter_crud_firebase/lib/main.dart
+++ flutter_crud_firebase/lib/main.dart
@@ -2,16 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+    StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: Scaffold(body: Center(child: CircularProgressIndicator())),
+          );
+        }
+        return GetMaterialApp(
+          title: "Application",
+          debugShowCheckedModeBanner: false,
+          initialRoute: snapshot.data != null ? AppPages.INITIAL : Routes.LOGIN,
+          getPages: AppPages.routes,
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            fontFamily: 'inter',
+          ),
+        );
+      },
     ),
   );
 }
